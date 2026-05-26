71fbe66693d56434c5145033e8cce96a07078f65
valen4446 <valentinoserpero11@gmail.com>
2026-05-26 15:50:44 -0300

    cambios

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,16 @@ let totalTexto: string = preguntar('Total de la cuenta: ');
 let esEstudiante: string = preguntar('¿Son estudiantes de ORT? si/no: ');
 let personasTexto: string = preguntar('Dividir la cuenta entre: ');
 
-let totalPorPersona: number = 0; // COMPLETAR
-console.log(`Total por persona: $${totalPorPersona}`);
+
+if(esEstudiante === "si"){
+    let valorDescuento: number = (parseInt(totalTexto)) * 0.9;
+    let totalPorPersona: number = valorDescuento / parseInt(personasTexto);
+    let total: string = totalPorPersona.toString();
+    console.log(`Total por persona: $${totalPorPersona}`);
+} else {
+    let totalPorPersona: number = parseInt(totalTexto) / parseInt(personasTexto);
+    let total: string = totalPorPersona.toString();
+    console.log(`Total por persona: $${totalPorPersona}`);
+}
+
+
