bed619294769df1d51fa8ca71555bc3bb6e7a11e
valen4446 <valentinoserpero11@gmail.com>
2026-05-26 15:55:02 -0300

    cambios

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,20 @@ let personasTexto: string = preguntar('Dividir la cuenta entre: ');
 
 
 if(esEstudiante === "si"){
+
     let valorDescuento: number = (parseInt(totalTexto)) * 0.9;
+
     let totalPorPersona: number = valorDescuento / parseInt(personasTexto);
+
     let total: string = totalPorPersona.toString();
+
     console.log(`Total por persona: $${totalPorPersona}`);
+
 } else {
     let totalPorPersona: number = parseInt(totalTexto) / parseInt(personasTexto);
+
     let total: string = totalPorPersona.toString();
+
     console.log(`Total por persona: $${totalPorPersona}`);
 }
 
